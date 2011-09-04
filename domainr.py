f0ba153e21e4ccbcd49232086f6968f07226cc90
Dave Menninger <dave.menninger@gmail.com>
2011-09-04 11:57:03 -0400

    first commit

---
+++ domainr.py
@@ -0,0 +1,21 @@
+import urllib2
+import json
+
+def domainr_search_json(domainname):
+	requesturl = 'http://www.domai.nr/api/json/search?q='
+	requesturl += domainname
+	request = urllib2.Request(requesturl)
+	request.add_header('User-Agent', 'domainr.py/0.1')
+	opener = urllib2.build_opener()
+	response = opener.open(request).read()
+	objs = json.loads(response)
+	return objs
+
+def is_com_taken(domainr_json):
+	return domainr_json['results'][0]['availability'] == 'taken'
+
+def is_net_taken(domainr_json):
+	return domainr_json['results'][1]['availability'] == 'taken'
+
+def is_org_taken(domainr_json):
+	return domainr_json['results'][2]['availability'] == 'taken'
